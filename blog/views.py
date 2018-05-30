aeb04a881e7c01ef3cb539dac1e90b5220cf7f96
kang <slash@zigbang.com>
2018-05-30 15:32:35 +0900

    debug_toolbar 설치하기

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,23 @@ from .models import Post
 def post_list(request):
     qs = Post.objects.all()
 
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(title__icontains =q)
+    id_search = request.GET.get('id-search', '')
+    if id_search:
+        qs = qs.filter(id=id_search)
+
+    title_search = request.GET.get('title-search', '')
+    if title_search:
+        qs = qs.filter(title__icontains=title_search)
+
+    date_search = request.GET.get('date-search', '')
+    if date_search:
+        qs = qs.filter(updated_at__icontains=date_search)
+
 
     return render(request, 'blog/post_list.html',{
-        'post_list': qs,
-    })
+        'qs': qs,
+        'id_search': id_search,
+        'title_search': title_search,
+        'date_search': date_search,
 
+    })
