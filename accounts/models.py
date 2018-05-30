a79e27a92806292dd57fc34317a9e5b74244d46e
kang <slash@zigbang.com>
2018-05-30 18:04:27 +0900

    Profile 모델 주소 테이블 이름 변경

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
-    adress = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
