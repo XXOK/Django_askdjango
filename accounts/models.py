f71772d93b9adbdf7954e9bc6099666f9a80bd2e
kang <slash@zigbang.com>
2018-05-31 16:39:59 +0900

    shop app 추가

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.address
