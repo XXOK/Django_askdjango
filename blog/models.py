6b3231684aa62d8f0a291a956245d5af845492e3
kang <slash@zigbang.com>
2018-06-18 15:31:07 +0900

    이미지 업로드/상세화면 이미지 노출 연습해보기

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요. 최대 100자 내외')
     content = models.TextField(verbose_name='내용') # 길이 제한이 없는 문자열
-    photo = models.ImageField(max_length=300, blank=True, upload_to='blog/post/%Y/%m/%d')
+    photo = models.ImageField(max_length=300, blank=True, upload_to='blog/post')
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, help_text='Ex: 127,35', blank=True, validators=[lnglat_validator])
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
