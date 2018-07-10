2abe58cb14b8a471ed5f3bac4d781c2fa0b90dcb
kang <slash@zigbang.com>
2018-07-10 16:32:53 +0900

    로그인 > 퀴즈 맞춰야 로그인 허용

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls import url
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.login, name='login',
-        kwargs={'template_name': 'accounts/login_form.html'}),
+        kwargs={
+            'template_name': 'accounts/login_form.html',
+            'authentication_form': LoginForm,
+        }),
     url(r'^logout/$', auth_views.logout, name='logout',
         kwargs={'next_page': settings.LOGIN_URL}),
 ]
