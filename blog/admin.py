a01015ec71e425d5d25e9502bcd4265a9ccfbaeb
kang <slash@zigbang.com>
2018-07-16 17:09:14 +0900

    django admin > prefetch_related 적용

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,16 @@ from .models import Post,Comment,Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'photo', 'status', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'tag_list', 'content_size', 'photo', 'status', 'created_at', 'updated_at']
 
     actions = ['make_draft', 'make_published', 'make_withdrawn']
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('tag_set')
+
+    def tag_list(request, post):
+        return ', '.join(tag.name for tag in post.tag_set.all())
 
     def content_size(self, Post):
         return '{}글자'.format(len(Post.content))
